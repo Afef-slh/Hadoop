49793c4a3893e877f45d801b3df38fe0669f16af
Afef-slh <80926132+Afef-slh@users.noreply.github.com>
2022-01-18 22:45:15 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ package tn.isima.wiem;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
